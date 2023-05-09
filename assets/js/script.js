1126801ac0abd8a89d90abca432fd059a66b48d8
Allan Dehors <allan@MacBook-Pro-de-Allan.local>
2023-05-09 13:35:33 +0200

    CSS/Responsive

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Déclaration variable
+
+let nb1 = document.getElementById('nb1')
+let ope = document.getElementById('ope')
+let nb2 = document.getElementById('nb2')
+let resultat = document.getElementById('resultat')
+let chiffre = document.getElementById('chiffre')
+
+chiffre.addEventListener('click', function(){
+    console.log('OK')
+})
